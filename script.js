5edc9843391b6f73553d4e36e7c612222d2837e2
Arhin <113555698+Arhinsingh05@users.noreply.github.com>
2025-01-06 09:45:44 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Menú hamburguesa
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('menu').classList.toggle('active');
+});
+
+// Desactivar clic derecho
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Desactivar teclas específicas (F12, Ctrl+Shift+I, Ctrl+U, etc.)
+document.addEventListener('keydown', function(e) {
+    if (e.key === "F12" || (e.ctrlKey && (e.key === 'U' || e.key === 'u' || e.key === 'I' || e.key === 'i'))) {
+        e.preventDefault();
+    }
+});
+
